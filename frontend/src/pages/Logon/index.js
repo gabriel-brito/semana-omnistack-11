b7e17fef41d0c4d53e9cc3a3d2d4fc41a8426085
Gabriel Brito <mfkrgabriel@gmail.com>
2020-03-28 13:43:16 -0300

    Add Login service

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import API from '../../services/api';
 
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
+
 export default function Logon(){
+  const [id, setId] = useState();
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    
+    try {
+      const response = await API.post('session', { id });
+      
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Seu login falhou. Tente novamente.')
+    }
+  };
+
   return (
     <div className='logon-container'>
       <section className='form'>
-        <img src={logoImg} alt='Be the Hero'/>
+        <img src={logoImg} alt='Be the Hero' />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type='text' placeholder='Seu ID'/>
+          <input
+            type='text'
+            placeholder='Seu ID'
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className='button' type='submit'>
             Entrar
           </button>
